e75f4fb1c42366f286a263e3edeb4d6600a6cd88
ENNOUKRA <ghaniennoukra@gmail.com>
2021-04-12 22:06:44 +0000

    fix last update time

--- time.js
+++ time.js
@@ -1,12 +1,12 @@
 function dateModification(page) {
-  if (document.getElementById("time")) {
+  if (document.getElementById("date")) {
     fetch("https://api.github.com/repos/ennoukrA/MySite/commits?path=" + page)
       .then((response) => {
         return response.json();
       })
       .then((commits) => {
         document.getElementById(
-          "time"
+          "date"
         ).textContent = commits[0].commit.committer.date
           .slice(0, 16)
           .split("T")
@@ -14,10 +14,6 @@ function dateModification(page) {
           .join("/");
       });
   }
-}
-
-function update(page) {
-  document.getElementById("time").textContent = "2021/11/22";
 }
 
 dateModification("index.html");
